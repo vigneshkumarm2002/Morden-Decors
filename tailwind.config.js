4d2735cfe907abaaeae3ab35bb77c861f2a761a0
VIGNESHKUMAR M <vigneshkumarlearner@gmail.com>
2023-07-12 18:12:31 +0530

    setup changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      colors:{
-        primary:"#053956" ,
-      },
-      fontFamily:{
-         poppins:["poppins"]
-      },
-      screens: {
-        'sd': '750px',
-        // => @media (min-width: 640px) { ... }
-  
-        'med': '1100px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
-
